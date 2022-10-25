6a18803a386096f47a5a6c53deec5293533a5d37
shockliang <shockwater@gmail.com>
2022-10-26 00:23:12 +0800

    One-pass hash table for go solution.

--- TwoSum/go-solution/solution.go
+++ TwoSum/go-solution/solution.go
@@ -4,18 +4,16 @@ func twoSum(nums []int, target int) []int {
 	numsMap := make(map[int]int)
 
 	for i := 0; i < len(nums); i++ {
-		numsMap[nums[i]] = i
-	}
-
-	for i := 0; i < len(nums); i++ {
 		complement := target - nums[i]
 
-		if idx, ok := numsMap[complement]; ok && idx != i {
+		if idx, ok := numsMap[complement]; ok {
 			indexes := make([]int, 2)
-			indexes[0] = i
-			indexes[1] = idx
+			indexes[0] = idx
+			indexes[1] = i
 			return indexes
 		}
+
+		numsMap[nums[i]] = i
 	}
 
 	return make([]int, 0)
