274571dd7fa9445fe2fefd6a13768617f79a3326
shockliang <shockwater@gmail.com>
2022-12-02 00:22:30 +0800

    Merge two sorted lists for csharp solution by iteration.

--- MergeTwoSortedLists/CSharpSolution/Solution.cs
+++ MergeTwoSortedLists/CSharpSolution/Solution.cs
@@ -19,23 +19,26 @@ public class Solution
 {
     public ListNode MergeTwoLists(ListNode list1, ListNode list2)
     {
-        if (list1 == null) return list2;
-        if (list2 == null) return list1;
-        
-        ListNode head = null;
-        if (list1.val < list2.val)
+        var head = new ListNode();
+        var tail = head;
+        while (list1 != null && list2 != null)
         {
-            head = list1;
-            list1 = list1.next;
-        }
-        else
-        {
-            head = list2;
-            list2 = list2.next;
+            if (list1.val < list2.val)
+            {
+                tail.next = list1;
+                list1 = list1.next;
+            }
+            else
+            {
+                tail.next = list2;
+                list2 = list2.next;
+            }
+
+            tail = tail.next;
         }
 
-        head.next = MergeTwoLists(list1, list2);
-        
-        return head;
+        tail.next = list1 == null ? list2 : list1;
+
+        return head.next;
     }
 }
